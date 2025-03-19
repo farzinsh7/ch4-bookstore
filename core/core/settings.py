9e87e5385da018836fa21de3dbe8926afde65a30
Farzin Shams <farzinnater@gmail.com>
2025-03-19 16:01:34 +0330

    Adding Crispy Forms to the project

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third-party Apps
 
+    # Third-party Apps
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     # Local Apps
     'accounts.apps.AccountsConfig',
@@ -156,3 +158,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = "pages:home"
 LOGOUT_REDIRECT_URL = "pages:home"
+
+
+# Crispy Configurations
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
